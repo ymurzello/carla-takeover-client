767f99f9f95b3b8dc3a8cfa46541571feae99c44
ymurzello <62921483+ymurzello@users.noreply.github.com>
2021-09-26 19:34:10 +0400

    Batch scripts
    to launch the server and client

---
+++ Launch server_client.bat
@@ -0,0 +1,32 @@
+@echo off
+
+title launch CARLA
+
+rem Start server
+d:
+cd "D:\Windows\CARLA\CARLA_0.9.12"
+start CarlaUE4.exe
+timeout 15
+cd "D:\Windows\CARLA\CARLA_0.9.12\PythonAPI\util"
+call python config.py -m Town04 
+
+timeout 10
+rem Launch client
+call "C:\ProgramData\Anaconda3\Scripts\activate.bat" "C:\ProgramData\Anaconda3"
+d:
+cd "D:\Windows\CARLA\Location A"
+start python synchronous_client.py -sp test5.json -sc scenario_configs/bike.json -r True
+
+timeout 10
+rem Change weather
+call "C:\ProgramData\Anaconda3\Scripts\activate.bat" "C:\ProgramData\Anaconda3"
+d:
+cd "D:\Windows\CARLA\CARLA_0.9.12\PythonAPI\util"
+call python environment.py --clouds 10 --wind 25 --sun day --weather clear --fog 0.0
+
+timeout 5
+rem Launch npc
+call "C:\ProgramData\Anaconda3\Scripts\activate.bat" "C:\ProgramData\Anaconda3"
+d:
+cd "D:\Windows\CARLA\Location A"
+call python npc_manager.py -sp test5.json
